be091edbc0e906d50db6d79d1f0691a83e1aa121
aishwarya0608 <aishwaryasurendra98@gmail.com>
2024-05-03 10:16:12 +0530

    collection task updated

---
+++ Day 3/Collection_task/secondproject/Employee2.java
@@ -0,0 +1,35 @@
+package secondproject;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Employee2 {
+    public static void main(String args[]) {
+        LinkedList<Employee> ll = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter the Employee Name");
+            String name = sc.next();
+            System.out.println("Enter the Employee id");
+            int id = sc.nextInt();
+            System.out.println("Enter the Employee salary");
+            double salary = sc.nextDouble();
+            ll.add(new Employee(id, name, salary));
+            System.out.println("Do you want to enter Employee Details");
+            if (!sc.next().equalsIgnoreCase("yes"))
+                break;
+        }
+
+       
+        Collections.sort(ll, Comparator.comparingDouble(Employee::getsalary));
+        //Collections.sort(ll, Comparator.comparingDouble(Employee::getid));
+        
+        for (Employee emp : ll) {
+            System.out.println(emp);
+        }
+    }
+}
